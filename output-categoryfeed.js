d8c552a64a7bc838e1ae74dae465b587a8b0ba71
Vic Chimenti <victor.chimenti@gmail.com>
2022-09-19 08:44:00 -0700

    fulltext

--- output-categoryfeed.js
+++ output-categoryfeed.js
@@ -204,8 +204,9 @@
              icons: getContentValues('<t4 type="content" name="Icon ID" output="normal" modifiers="striptags,htmlentities" />'),
              lsapIcons: getContentValues('<t4 type="content" name="LSAP ID" output="normal" modifiers="striptags,htmlentities" />'),             
              summaryDescription: getContentValues('<t4 type="content" name="Plaintext Description" output="normal" modifiers="striptags,htmlentities" />'),
-             fullTextLink: getContentValues('<t4 type="content" name="Article Title" output="fulltext" use-element="true" filename-element="Article Title" modifiers="striptags,htmlentities" />'),
+
              
+             fullTextLink: getContentValues('<t4 type="content" name="Title" output="fulltext" use-element="true" filename-element="Title" modifiers="striptags,htmlentities" />'),             
              contentId: getContentValues('<t4 type="meta" meta="content_id" />')
  
          };
