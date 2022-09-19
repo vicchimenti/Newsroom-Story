1acb271c5fad4757e10dc020bc421182bd3028b8
Vic Chimenti <victor.chimenti@gmail.com>
2022-09-19 09:21:56 -0700

    title link aria

--- output-categoryfeed.js
+++ output-categoryfeed.js
@@ -226,7 +226,7 @@
           * */
           let closeTitle = '</h2>';
           let titleWrapper = (majorNewsDict.fullTextLink.content && majorNewsDict.headline.content) ?
-            '<h2 class="newsroomArticleTitle"><a href="' + majorNewsDict.fullTextLink.content + '" aria-label="' + majorNewsDict.headline.content + '"></a>' :
+            '<h2 class="newsroomArticleTitle"><a href="' + majorNewsDict.fullTextLink.content + '" aria-label="Read the full article: ' + majorNewsDict.headline.content + '"></a>' :
             (majorNewsDict.contentId.content && !majorNewsDict.headline.content) ?
             '<article class="newsroomArticleWrapper newsroomBlurb contentItem" id="id' + majorNewsDict.contentId.content + 'category" aria-label="' + majorNewsDict.contentName.content + '">' :
             '<article class="newsroomArticleWrapper newsroomBlurb contentItem">';
